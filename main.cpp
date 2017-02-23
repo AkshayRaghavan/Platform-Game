5f127a5ebb01ac48a8fc76bd9084ad0ec12a3a6a
GoodDeeds <raosukrut@gmail.com>
2017-02-23 20:56:09 +0530

    update main with timer

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <QApplication>
+#include <QTimer>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QTimer *timer = new QTimer(this);
+    timer->setInterval(1000/33);
+    ReadInput readInput;
+    GameState *gameState = readInput.createGameStateObject();
+    connect(timer,SIGNAL(timeout()),gameState,SLOT(update()));
     return a.exec();
 }
