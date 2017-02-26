fa72be58aee23957f5190b292e4b9de3ec5c15bd
atulraj297 <cs15btech11006@iith.ac.in>
2017-02-27 00:03:29 +0530

    Removed the rect coming accross door

--- src/door.cpp
+++ src/door.cpp
@@ -1,7 +1,16 @@
 #include "door.h"
+#include <QPen>
 
 Door::Door(qreal x, qreal y, qreal w, qreal h, QGraphicsScene * scene)
 {
-    this->setRect(x,y,w,h);
-    scene -> addItem(this);
+    QPen pen;
+    pen.setColor(Qt::transparent);
+    this->x = x;
+    this->y = y;
+    this->w = w;
+    this->h = h;
+    /*this->setRect(x,y,w,h);
+    scene -> addItem(this);*/
+    this->r = scene->addRect(x,y,w,h,pen);
+
 }
