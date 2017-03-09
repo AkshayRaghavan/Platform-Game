73018674c99cb7e1a8dfd612460b21847a61753a
atulraj297 <cs15btech11006@iith.ac.in>
2017-03-09 17:14:16 +0530

    Add Comments on Header Files

--- include/button.h
+++ include/button.h
@@ -6,19 +6,51 @@
 #include <QPixmap>
 #include <QDebug>
 
+/*!
+ * \brief All the buttons displayed in Main Menu
+ */
+
 class Button:public QObject, public QGraphicsPixmapItem{
     Q_OBJECT
 private:
+    /*!
+     * \brief PixMap to be displayed when Mouse is away from button
+     */
     QPixmap idlePixMap;
+    /*!
+     * \brief PixMap to be displayed when Mouse is on the button
+     */
     QPixmap hoverPixMap;
 public:
     // constructors
-    Button(const char*  , const char*  , int  , int );
+    /*!
+     * \brief The Constructor for button class
+     * \param idle_image_path file path containing image for idlePixMap
+     * \param hover_image_path file path containing image for hoverPixMap
+     * \param screen_height Height of the Screen
+     * \param screen_width Width of the Screen
+     */
+    Button(const char* idle_image_path , const char* hover_image_path , int screen_height , int screen_width);
     // public methods (events)
+    /*!
+     * \brief Function called when button gets clicked
+     * \param event The Event received
+     */
     void mousePressEvent(QGraphicsSceneMouseEvent *event);
+    /*!
+     * \brief Function called when mouse cursor touches the button
+     * \param event The Event received
+     */
     void hoverEnterEvent(QGraphicsSceneHoverEvent *event);
+    /*!
+     * \brief Function called when mouse cursor leaves the button
+     * \param event The Event received
+     */
     void hoverLeaveEvent(QGraphicsSceneHoverEvent *event);
 signals:
+    /*!
+     * \brief Signal to notify if button is clicked
+     */
     void clicked();
 };
 
--- include/readinput.h
+++ include/readinput.h
@@ -31,29 +31,100 @@
 #include <thread>
 #include <QApplication>
 
+/*!
+ * \brief Function to read input from the game files
+ */
+
 class ReadInput
 {
 
 public:
+    /*!
+     * \brief Stores whether it is Server or Client
+     */
     enumerator::Identity remoteIdentity;
+    /*!
+     * \brief Vector Containing all the Game Objects
+     */
     std::vector<GameObject*> gameObject;
+    /*!
+     * \brief The Tile Map Vector
+     */
     std::vector< std::vector<Tile*> > tileMap;
+    /*!
+     * \brief vector to store all the gems
+     */
     std::vector<Gem*> gems;
+    /*!
+     * \brief The Scene where to add all the game objects
+     */
     QGraphicsScene * scene;
+    /*!
+     * \brief Width of the Tile relative to Screen Width
+     */
     qreal width_of_tile;
+    /*!
+     * \brief Height of the Tile relative to Screen Height
+     */
     qreal height_of_tile;
+    /*!
+     * \brief QApplication
+     */
     QApplication * app;
+    /*!
+     * \brief Width of the Screen
+     */
     int screenWidth;
+    /*!
+     * \brief Height of the Screen
+     */
     int screenHeight;
+    /*!
+     * \brief Total Time before Game Ends
+     */
     int totalTime;
 
+    /*!
+     * \brief Constructor
+     * \param scene The Scene pointer
+     * \param screen_width Value of Width of Screen
+     * \param screen_height Value of Height of Screen
+     */
     ReadInput (QGraphicsScene * scene, int screen_width, int screen_height);
+    /*!
+     * \brief Function to Create the Tile Map
+     * \param file_path Path of File containing information of Tile Map
+     */
     void functionToCreateTileMap(std::string file_path);
+    /*!
+     * \brief Function to Create Gems
+     * \param file_path Path of the File containing information about Gems
+     */
     void functionToCreateGem(std::string file_path);
-    void functionToCreatePlayerGameObject(std::string);
-    void functionToCreateMonsterGameObject(std::string );
-    void functionToCreateFireObject(std::string );
-    void functionToCreateDoor(std::string );
+    /*!
+     * \brief Function to Create The Players
+     * \param file_path Path of the File containing all the information about players
+     */
+    void functionToCreatePlayerGameObject(std::string file_path);
+    /*!
+     * \brief Function to Create the Monsters
+     * \param file_path Path of the File Containing monsters
+     */
+    void functionToCreateMonsterGameObject(std::string file_path );
+    /*!
+     * \brief Function to Create Fire
+     * \param fire_file_path Path of the File containing Fire
+     */
+    void functionToCreateFireObject(std::string fire_file_path );
+    /*!
+     * \brief functionToCreateDoor
+     * \param door_file_path Path of the File containing Door information
+     */
+    void functionToCreateDoor(std::string door_file_path );
+    /*!
+     * \brief To set the Current QApplication
+     * \param QApplication Pointer
+     */
     void setApp(QApplication *a);
 
 };
